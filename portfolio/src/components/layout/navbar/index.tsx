e0037521782a17efda069c3b1dd34d3bae08564e
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-06 13:24:56 +0100

    feat: :sparkles: Added some shadcn ui components

---
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <header>
+      <div className="flex items-center justify-between h-12 w-full">
+        <p>Home</p>
+        <p>Contact</p>
+        <p>Lol</p>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
