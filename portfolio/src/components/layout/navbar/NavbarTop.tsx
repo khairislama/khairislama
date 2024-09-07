aa6c2f413eaff5e8ee8c2e0c38590b4b22172f80
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-07 17:27:10 +0100

    fix: :bug: fixed navbar top

--- portfolio/src/components/layout/navbar/NavbarTop.tsx
+++ portfolio/src/components/layout/navbar/NavbarTop.tsx
@@ -4,10 +4,12 @@ export const runtime = "edge";
 
 export default function NavbarTop() {
   return (
-    <div className="flex items-center justify-center lg:justify-end lg:pr-6 gap-8 max-w-7xl mx-auto h-6 lg:h-8 border-b-2 border-border text-xs">
-      <span className="cursor-default">54 007 387</span>
-      <span className="cursor-pointer"> Download CV </span>
-      <NavLanguage />
+    <div className="h-6 lg:h-8 border-b-2 border-border w-full">
+      <div className="flex items-center justify-center lg:justify-end lg:pr-6 gap-8 max-w-7xl mx-auto text-xs">
+        <span className="cursor-default">54 007 387</span>
+        <span className="cursor-pointer"> Download CV </span>
+        <NavLanguage />
+      </div>
     </div>
   );
 }
