90bb4dfe60b87d37b4c7c0ace2bc8ce0bb27f8e3
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-07 18:00:08 +0100

    feat: :sparkles: navbar colors

--- portfolio/src/components/layout/navbar/index.tsx
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -2,7 +2,7 @@ import NavbarTop from "./NavbarTop";
 
 function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full bg-navbar-background text-navbar-foreground">
+    <header className="sticky top-0 z-50 w-full bg-navbar-background text-navbar-foreground drop-shadow-xl">
       <NavbarTop />
       <nav className="flex items-center justify-between h-10 md:h-12 lg:h-14 border-b-2 border-border backdrop-blur-lg bg-opacity-70 hover:bg-opacity-100 focus-within:bg-opacity-100 transition-all ease-in-out duration-300">
         <p>Home</p>
