5cc3cc93fb979b357c72762949893ab7d9acfb20
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-07 13:25:18 +0100

    feat: :sparkles: added navbar top

--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   const translations = useTranslations("Navbar");
   return (
     <div className="flex flex-col">
-      <div className="h-12 bg-red-200 w-full"> {translations("home")} </div>
-      <div className="h-12 bg-green-200 w-full"></div>
+      <div className="h-96 bg-red-200 w-full"> {translations("home")} </div>
+      <div className="h-96 bg-green-200 w-full"></div>
     </div>
   );
 }
