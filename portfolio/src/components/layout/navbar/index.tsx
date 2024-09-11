7e8525fa79345b5c3c9377bac25862219250afa6
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-11 15:15:38 +0100

    feat: :sparkles: added dark mode

--- portfolio/src/components/layout/navbar/index.tsx
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   const translations = useTranslations("Navbar");
 
   return (
-    <header className="sticky top-0 z-50 w-full bg-navbar-background text-navbar-foreground drop-shadow-xl">
+    <header className="sticky top-0 z-50 w-full drop-shadow-xl">
       <NavbarTop />
       <nav className="h-10 md:h-12 lg:h-14 border-b-2 border-border bg-opacity-70 hover:bg-opacity-100 focus-within:bg-opacity-100 transition-all ease-in-out duration-300">
         <div className="flex items-center justify-between max-w-7xl mx-auto">
