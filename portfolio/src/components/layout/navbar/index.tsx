00c7eacf092cbade3a20a507164bfc52a3a48611
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-11 22:24:17 +0100

    fix: :bug: Fixed svgs

--- portfolio/src/components/layout/navbar/index.tsx
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -4,7 +4,7 @@ import SocialIcons from "./SocialIcons";
 
 function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full drop-shadow-xl bg-background/70 hover:bg-background focus-within:bg-background transition-all ease-in-out duration-300">
+    <header className="sticky top-0 z-50 w-full drop-shadow-xl bg-background/70 hover:bg-background focus-within:bg-background transition-all ease-in-out duration-300 font-rubik">
       <NavbarTop />
       <div className="h-10 md:h-12 lg:h-14 border-b-2 border-foreground/70">
         <div className="relative flex items-center justify-between max-w-7xl mx-auto">
