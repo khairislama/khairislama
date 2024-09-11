fee525611fa74696e7f46e704f7f492b66e5a00b
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-11 22:20:48 +0100

    feat: :sparkles: separate components

--- portfolio/src/components/layout/navbar/index.tsx
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -1,40 +1,20 @@
-import { useTranslations } from "next-intl";
 import NavbarTop from "./NavbarTop";
-import Image from "next/image";
+import Navigation from "./Navigation";
+import SocialIcons from "./SocialIcons";
 
 function Navbar() {
-  const translations = useTranslations("Navbar");
-
   return (
     <header className="sticky top-0 z-50 w-full drop-shadow-xl bg-background/70 hover:bg-background focus-within:bg-background transition-all ease-in-out duration-300">
       <NavbarTop />
-      <nav className="h-10 md:h-12 lg:h-14 border-b-2 border-white/70">
-        <div className="flex items-center justify-between max-w-7xl mx-auto">
-          <div className="flex items-center justify-center gap-10">
-            <p>{translations("home")}</p>
-            <p>{translations("about")}</p>
-            <p>{translations("play")}</p>
-            <p>{translations("projects")}</p>
-            <p>{translations("contact")}</p>
-          </div>
-          <div className="flex items-center justify-center gap-8">
-            <Image
-              src="/icons/social/instagram.svg"
-              alt="instagram carthabot"
-              width={20}
-              height={20}
-              className="w-8"
-            />
-            <Image
-              src="/icons/social/github.svg"
-              alt="github carthabot"
-              width={20}
-              height={20}
-              className="w-8"
-            />
+      <div className="h-10 md:h-12 lg:h-14 border-b-2 border-foreground/70">
+        <div className="relative flex items-center justify-between max-w-7xl mx-auto">
+          <Navigation />
+          <SocialIcons />
+          <div className="absolute border-2 border-border top-0 right-1/2 p-2 rounded-full">
+            KS
           </div>
         </div>
-      </nav>
+      </div>
     </header>
   );
 }
