e73a2eb23d1034b1384dfa71f6b22c2f3620500a
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-11 15:11:02 +0100

    fix: :bug: removed old colors

--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="h-96 bg-red-200 w-full"> {translations("home")} </div>
-      <div className="h-96 bg-green-200 w-full"></div>
+      <div className="h-96 bg-green-200 w-full text-black">
+        <h1 className="text-3xl font-bold">HELLLOOO there how are YOU gg</h1>
+        <h1 className="text-3xl font-bold font-rubik">
+          HELLLOOO there how are YOU gg
+        </h1>
+        <h1 className="text-3xl font-bold font-syne">
+          HELLLOOO there how are YOU gg
+        </h1>
+      </div>
     </div>
   );
 }
