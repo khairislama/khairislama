6af5c12b0eb05a067f062b9e3c40124abe313fbf
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 15:14:26 +0100

    fix: :bug: fixed some colors

--- portfolio/src/components/layout/know-about-me/index.tsx
+++ portfolio/src/components/layout/know-about-me/index.tsx
@@ -5,14 +5,17 @@ import Picture from "./Picture";
 function KnowAboutMe() {
   const translations = useTranslations("KnowMe");
   return (
-    <section className="relative w-full mt-16">
+    <section className="relative w-full my-16">
       <div className="max-w-7xl mx-auto grid grid-cols-2 gap-24">
         <div className="my-auto flex flex-col gap-14">
           <h2 className="font-syne font-bold text-5xl max-w-md">
             {" "}
             {translations("title")}{" "}
           </h2>
-          <p className="font-rubik"> {translations("description")} </p>
+          <p className="font-rubik text-justify">
+            {" "}
+            {translations("description")}{" "}
+          </p>
           <Button
             size={"lg"}
             className="h-12 bg-gradient-to-r from-primary to-secondary text-foreground/80 font-rubik w-1/2 rounded-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
