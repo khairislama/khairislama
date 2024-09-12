9340f3523537648d11423cc6e1cdf3edaf3920cf
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 23:12:36 +0100

    feat: :sparkles: added skills

---
+++ portfolio/src/lib/Skills.ts
@@ -0,0 +1,12 @@
+interface ISkill {
+  name: string;
+  width: number;
+  height: number;
+}
+
+export const Skills: ISkill[] = [
+  { name: "next", width: 46, height: 21 },
+  { name: "react", width: 44, height: 39 },
+  { name: "tailwindcss", width: 44, height: 43 },
+  { name: "node", width: 44, height: 29 },
+];
