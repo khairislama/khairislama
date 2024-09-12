e1abd7651bbbb3a6deb0820776c93064e5d19174
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 16:32:18 +0100

    feat: :sparkles: responsive design

--- portfolio/src/components/layout/hero/index.tsx
+++ portfolio/src/components/layout/hero/index.tsx
@@ -8,10 +8,10 @@ function Hero() {
 
   return (
     <section className="relative w-full font-syne">
-      <div className="flex flex-col items-center justify-between max-w-3xl mx-auto mt-16 pb-16 z-20">
+      <div className="flex flex-col items-center justify-between max-w-3xl mx-auto mt-10 md:mt-14 lg:mt-16 pb-16 z-20">
         <h1 className="text-3xl"> {translations("welcome")} </h1>
         <Picture />
-        <h2 className="text-center text-7xl uppercase font-bold text-foreground/70 mt-10">
+        <h2 className="text-center text-5xl sm:text-6xl lg:text-7xl uppercase font-bold text-foreground/70 mt-10">
           {translations("title")}
         </h2>
       </div>
@@ -21,14 +21,14 @@ function Hero() {
         alt="eclipse"
         width={1330}
         height={288}
-        className="absolute inset-0 object-cover w-full pointer-events-none"
+        className="absolute object-cover w-full pointer-events-none bottom-[7%] sm:bottom-0 xl:inset-0"
       />
       <Image
         src="/hero/background2.webp"
         alt="curvy lines"
         width={1200}
         height={1200}
-        className="absolute -bottom-[90%] object-cover opacity-30 w-full pointer-events-none"
+        className="hidden lg:block absolute -bottom-[90%] object-cover opacity-30 w-full pointer-events-none"
       />
     </section>
   );
