69290401fb086a82608d3ce41294c1a51bd44d57
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 13:11:41 +0100

    feat: :sparkles: fixed hero layoum and images brightness

--- portfolio/src/components/layout/hero/Picture.tsx
+++ portfolio/src/components/layout/hero/Picture.tsx
@@ -8,21 +8,21 @@ function Picture() {
         alt="eclipse 2"
         width={200}
         height={200}
-        className="absolute inset-0 z-10 backdrop-blur-sm object-cover pointer-events-none"
+        className="absolute inset-0 z-10 object-cover pointer-events-none blur-lg brightness-200"
       />
       <Image
         src="/hero/eclipse1.svg"
         alt="eclipse 1"
         width={384}
         height={384}
-        className="absolute inset-0 z-20 drop-shadow-2xl backdrop-blur-sm pointer-events-none"
+        className="absolute inset-0 z-20 drop-shadow-2xl pointer-events-none blur-lg brightness-200"
       />
       <Image
         src="/hero/gradient.svg"
         alt="gradient"
         width={196}
         height={196}
-        className="absolute inset-0 z-30 pointer-events-none"
+        className="absolute inset-0 z-30 pointer-events-none brightness-200"
       />
       <div className="absolute z-40 inset-0 m-auto h-32 w-32 rounded-full border-2 border-border overlay-hidden">
         <Image
@@ -33,7 +33,7 @@ function Picture() {
           className="h-full w-full object-cover rounded-full"
         />
       </div>
-      <p className="z-40 absolute bottom-0 left-[50%] translate-x-[-50%] text-2xl">
+      <p className="z-40 absolute bottom-0 left-[50%] translate-x-[-50%] text-2xl pointer-events-none">
         👋
       </p>
     </div>
--- portfolio/src/components/layout/navbar/index.tsx
+++ portfolio/src/components/layout/navbar/index.tsx
@@ -5,7 +5,7 @@ import SocialIcons from "./SocialIcons";
 
 function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full drop-shadow-xl bg-background/70 hover:bg-background focus-within:bg-background transition-all ease-in-out duration-300 font-rubik">
+    <header className="sticky top-0 z-50 w-full drop-shadow-xl bg-background/70 hover:bg-background backdrop-blur-sm focus-within:bg-background transition-all ease-in-out duration-300 font-rubik">
       <NavbarTop />
       <div className="h-10 md:h-12 lg:h-14 border-b-2 border-foreground/70">
         <div className="relative flex items-center justify-between max-w-7xl mx-auto">
