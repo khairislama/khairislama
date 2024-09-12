04dc0c2374610a64b4d0bce7a6074e7eaeb66c31
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 23:12:36 +0100

    feat: :sparkles: added skills

---
+++ portfolio/src/components/layout/skills/SkillsGroup.tsx
@@ -0,0 +1,17 @@
+function SkillsGroup() {
+  return (
+    <div className="max-w-4xl mx-auto flex mt-16 items-center justify-center">
+      <div className="w-24 h-24 bg-foreground/5 drop-shadow-md rounded-full flex items-center justify-center">
+        {/* <Image
+          src="/shapes/eclipse-tiny.svg"
+          alt="next js"
+          width={1330}
+          height={288}
+          className="absolute object-cover w-full pointer-events-none -top-[0%] md:-top-[50%] xl:-top-[100%] 2xl:-top-[150%] -z-10"
+        /> */}
+      </div>
+    </div>
+  );
+}
+
+export default SkillsGroup;
