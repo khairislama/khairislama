82771c1186708edf9e52fb1662e3a05a2a848dc1
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-12 23:19:13 +0100

    feat: :sparkles: added skills images

--- portfolio/src/lib/Skills.ts
+++ portfolio/src/lib/Skills.ts
@@ -9,4 +9,13 @@ export const Skills: ISkill[] = [
   { name: "react", width: 44, height: 39 },
   { name: "tailwindcss", width: 44, height: 43 },
   { name: "node", width: 44, height: 29 },
+  { name: "postgresql", width: 36, height: 34 },
+  { name: "framer-motion", width: 36, height: 34 },
+  { name: "typescript", width: 36, height: 34 },
+  { name: "nestjs", width: 36, height: 34 },
+  { name: "figma", width: 29, height: 39 },
+  { name: "git", width: 36, height: 36 },
+  { name: "python", width: 36, height: 34 },
+  { name: "express", width: 54, height: 15 },
+  { name: "mongodb", width: 51, height: 12 },
 ];
