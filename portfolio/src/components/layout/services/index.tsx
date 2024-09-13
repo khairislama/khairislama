b534b4c63ba4d4669a05491dbf4e773eb5427266
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-13 18:04:47 +0100

    feat: :sparkles: Started working on services and leave projects for later

---
+++ portfolio/src/components/layout/services/index.tsx
@@ -0,0 +1,24 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+function Services() {
+  const translations = useTranslations("Services");
+  return (
+    <section className="relative w-full">
+      <Image
+        src="/shapes/eclipse-tiny.svg"
+        alt="eclipse"
+        width={1330}
+        height={288}
+        className="absolute object-cover w-full pointer-events-none -top-[0%] xl:-top-[100%] 2xl:-top-[170%] 2k:-top-[200%] -z-10 brightness-125"
+      />
+      <div className="max-w-7xl mx-auto flex flex-col p-10 xl:p-0 text-center">
+        <h2 className="relative text-5xl sm:text-6xl lg:text-7xl font-syne font-bold text-primary/50 mt-0 md:mt-10">
+          {translations("title")}
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default Services;
