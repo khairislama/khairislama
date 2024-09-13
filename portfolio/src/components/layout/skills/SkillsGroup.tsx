3e6a36b22d7770bcf48bc0aa2fc6478e6b95d503
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-13 11:02:34 +0100

    feat: :sparkles: start working on skills

--- portfolio/src/components/layout/skills/SkillsGroup.tsx
+++ portfolio/src/components/layout/skills/SkillsGroup.tsx
@@ -1,14 +1,17 @@
+import { SKILLS } from "@/lib/Skills";
+import Image from "next/image";
+
 function SkillsGroup() {
   return (
     <div className="max-w-4xl mx-auto flex mt-16 items-center justify-center">
       <div className="w-24 h-24 bg-foreground/5 drop-shadow-md rounded-full flex items-center justify-center">
-        {/* <Image
-          src="/shapes/eclipse-tiny.svg"
+        <Image
+          src={`/skills/${SKILLS[3].name}.png`}
           alt="next js"
-          width={1330}
-          height={288}
-          className="absolute object-cover w-full pointer-events-none -top-[0%] md:-top-[50%] xl:-top-[100%] 2xl:-top-[150%] -z-10"
-        /> */}
+          width={SKILLS[3].width}
+          height={SKILLS[3].height}
+          className=""
+        />
       </div>
     </div>
   );
