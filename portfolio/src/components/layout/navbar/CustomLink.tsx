3554d5aabeb3aed6ba1cf1ac1eb1cf17ddcb8529
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-16 16:28:36 +0100

    fix: :bug: fixed active links and navigation

--- portfolio/src/components/layout/navbar/CustomLink.tsx
+++ portfolio/src/components/layout/navbar/CustomLink.tsx
@@ -13,16 +13,26 @@ const CustomLink = ({
   className?: string;
 }) => {
   const pathname = usePathname();
+  const langPrefix = pathname.split("/")[1];
+
+  // Build the full link with the language prefix
+  const fullLink = `/${langPrefix}${href === "/" ? "" : href}`;
+
+  // Check if the current path matches the link (ignoring the language prefix)
+  const isActive =
+    pathname === fullLink || (href === "/" && pathname === `/${langPrefix}`);
   return (
     <Link
-      href={href}
-      className={`${className} relative group hover:text-foreground`}
+      href={fullLink}
+      className={`${className} relative group hover:text-foreground ${
+        isActive && "text-foreground"
+      }`}
     >
       {title}
 
       <span
         className={`h-[1px] inline-block w-0 bg-foreground absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
-          pathname === href ? "w-full" : "w-0"
+          isActive ? "w-full" : "w-0"
         }`}
       >
         &nbsp;
