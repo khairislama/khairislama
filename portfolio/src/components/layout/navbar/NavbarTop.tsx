913180462ffb0248a474aa672ee2bd7952fa74ac
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-16 16:25:05 +0100

    fix: :bug: fixed social icons and added links (removed download cv for dev)

--- portfolio/src/components/layout/navbar/NavbarTop.tsx
+++ portfolio/src/components/layout/navbar/NavbarTop.tsx
@@ -11,7 +11,7 @@ export default function NavbarTop() {
     <div className="h-6 lg:h-8 w-full">
       <div className="h-full flex items-center justify-center lg:justify-end gap-8 max-w-7xl mx-auto text-xs">
         <span className="cursor-default">+ 216 54 00 73 87</span>
-        <Link href="/cv-francais.pdf" download className="cursor-pointer">
+        <Link href="" className="cursor-pointer">
           {translations("cv")}
         </Link>
         <NavLanguage />
