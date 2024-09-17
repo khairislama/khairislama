e5e4bbd5f7d0a536001e5cbcb7ebb8ced93414cb
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-17 13:20:58 +0100

    feat: :sparkles: footer added

---
+++ portfolio/src/components/layout/footer/Circle.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+function Circle() {
+  return (
+    <div className="w-48 h-auto flex items-center justify-center relative rounded-full border-2 border-foreground/70 border-dashed p-3">
+      <svg
+        viewBox="0 0 100 100"
+        className="w-full h-full fill-foreground tracking-wider"
+      >
+        <path
+          id="circlePath"
+          d="M 50, 50 m -40, 0 a 40,40 0 1,1 80,0 a 40,40 0 1,1 -80,0"
+          fill="none"
+        />
+        <text className="text-sm">
+          <textPath
+            xlinkHref="#circlePath"
+            startOffset="50%"
+            textAnchor="middle"
+          >
+            Khairi SLAMA ** Web Developer ***
+          </textPath>
+        </text>
+      </svg>
+      <div className="w-28 h-28 flex items-center justify-center rounded-full overflow-hidden border-foreground/70 border-2 border-dashed absolute left-[50%] translate-x-[-50%] top-[20%]">
+        <Image
+          src="/avatars/Avatar-7.png"
+          alt="@khairislama"
+          width={100}
+          height={100}
+          className="w-full"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Circle;
---
+++ portfolio/src/components/layout/footer/index.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Circle from "./Circle";
+import { useTranslations } from "next-intl";
+
+function Footer() {
+  const translations = useTranslations("Footer");
+  return (
+    <footer className="mt-10 bg-gradient-to-b from-background via-primary/50 to-primary/70 w-full pb-10">
+      <div className="max-w-4xl mx-auto flex flex-col items-center justify-center overflow-hidden">
+        <Circle />
+        <div className="bg-background/50 mt-14 p-3 rounded-xl border-foreground/70 border shadow-xl">
+          <h4 className="text-xs flex items-center justify-center gap-2">
+            <span> {translations("cooked with love in")} </span>
+            <Image
+              src="/icons/tunisia.png"
+              alt="Tunisia"
+              width={64}
+              height={43}
+              className="w-5"
+            />
+            <span> {translations("with")} </span>{" "}
+            <Image
+              src="/icons/social/figma.svg"
+              alt="Figma"
+              width={40}
+              height={40}
+              className="w-4"
+            />
+            <span>&</span>{" "}
+            <Image
+              src="/icons/nextjs.png"
+              alt="Next JS"
+              width={62}
+              height={62}
+              className="w-4"
+            />
+          </h4>
+        </div>
+        <p className="text-xs mt-4 text-foreground/70">
+          &copy; Khairi SLAMA 2024
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
