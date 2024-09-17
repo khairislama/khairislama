b5ae185fb152970eb182727b49b2dfdcc08eb29d
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-17 22:51:16 +0100

    feat: :sparkles: worked on services info

--- portfolio/src/components/layout/services/index.tsx
+++ portfolio/src/components/layout/services/index.tsx
@@ -16,20 +16,25 @@ function Services() {
       <div className="max-w-4xl mx-auto flex flex-col p-10 xl:p-0 text-center">
         <h2 className="text-header">{translations("title")}</h2>
         <div className="grid grid-cols-5 gap-2 mt-12">
-          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
-            <Check className="h-5 w-5" /> <span>Design System</span>
+          <div className="p-2 bg-background/40 flex items-center justify-center rounded-2xl gap-1 shadow-lg">
+            <Check className="h-5 w-6 text-[#B084FF]" />{" "}
+            <span>Design System</span>
           </div>
-          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
-            <Check className="h-5 w-5" /> <span>Design System</span>
+          <div className="p-2 bg-background/40 flex items-center justify-center rounded-2xl gap-1 shadow-lg">
+            <Check className="h-5 w-6 text-[#B084FF]" />{" "}
+            <span>Design System</span>
           </div>
-          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
-            <Check className="h-5 w-5" /> <span>Design System</span>
+          <div className="p-2 bg-background/40 flex items-center justify-center rounded-2xl gap-1 shadow-lg">
+            <Check className="h-5 w-6 text-[#B084FF]" />{" "}
+            <span>Design System</span>
           </div>
-          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
-            <Check className="h-5 w-5" /> <span>Design System</span>
+          <div className="p-2 bg-background/40 flex items-center justify-center rounded-2xl gap-1 shadow-lg">
+            <Check className="h-5 w-6 text-[#B084FF]" />{" "}
+            <span>Design System</span>
           </div>
-          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
-            <Check className="h-5 w-5" /> <span>Design System</span>
+          <div className="p-2 bg-background/40 flex items-center justify-center rounded-2xl gap-1 shadow-lg">
+            <Check className="h-5 w-6 text-[#B084FF]" />{" "}
+            <span>Design System</span>
           </div>
         </div>
       </div>
