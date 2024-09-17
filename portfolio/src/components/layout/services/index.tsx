0bf82540b3ececc53d4722d17d16b1db38c54584
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-17 14:04:30 +0100

    feat: :construction: started working on services

--- portfolio/src/components/layout/services/index.tsx
+++ portfolio/src/components/layout/services/index.tsx
@@ -1,3 +1,4 @@
+import { Check } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
@@ -12,8 +13,25 @@ function Services() {
         height={288}
         className="absolute object-cover w-full pointer-events-none -top-[0%] xl:-top-[100%] 2xl:-top-[170%] 2k:-top-[200%] -z-10 brightness-125"
       />
-      <div className="max-w-7xl mx-auto flex flex-col p-10 xl:p-0 text-center">
+      <div className="max-w-4xl mx-auto flex flex-col p-10 xl:p-0 text-center">
         <h2 className="text-header">{translations("title")}</h2>
+        <div className="grid grid-cols-5 gap-2 mt-12">
+          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
+            <Check className="h-5 w-5" /> <span>Design System</span>
+          </div>
+          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
+            <Check className="h-5 w-5" /> <span>Design System</span>
+          </div>
+          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
+            <Check className="h-5 w-5" /> <span>Design System</span>
+          </div>
+          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
+            <Check className="h-5 w-5" /> <span>Design System</span>
+          </div>
+          <div className="p-2 bg-background/50 flex items-center justify-center rounded-2xl">
+            <Check className="h-5 w-5" /> <span>Design System</span>
+          </div>
+        </div>
       </div>
     </section>
   );
