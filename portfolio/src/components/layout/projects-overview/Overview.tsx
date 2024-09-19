47053a6cfd9de2e370d3461ba42f27f99374a6a2
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-19 23:03:17 +0100

    feat: :sparkles: added description for projects

--- portfolio/src/components/layout/projects-overview/Overview.tsx
+++ portfolio/src/components/layout/projects-overview/Overview.tsx
@@ -15,7 +15,7 @@ interface Props {
 function Overview({ reverse, projectNumber, image }: Props) {
   return (
     <div
-      className={`flex flex-col relative md:mt-24 ${
+      className={`flex flex-col relative md:mt-24 md:hover:scale-105 duration-300 ease-in-out transition-all ${
         reverse ? "md:flex-row-reverse" : "md:flex-row"
       }`}
     >
