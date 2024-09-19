21480cf6b7d1e41da4b3737224eb72caff3be9f3
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-19 09:04:53 +0100

    feat: :construction: working on the last 2 pieces of the landing page

--- portfolio/src/components/layout/testimonials/index.tsx
+++ portfolio/src/components/layout/testimonials/index.tsx
@@ -1,5 +1,35 @@
+import {
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import CarouselSettings from "./CarouselSettings";
+
 function Testimonials() {
-  return <div>daz</div>;
+  return (
+    <section>
+      {" "}
+      <CarouselSettings>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem
+              key={index}
+              className="lg:basis-1/3 p-1 bg-red-500 mx-1"
+            >
+              <div className="flex w-full h-44 items-center justify-center p-6">
+                <span className="text-3xl font-semibold">{index + 1}</span>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="hidden lg:block">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
+      </CarouselSettings>
+    </section>
+  );
 }
 
 export default Testimonials;
