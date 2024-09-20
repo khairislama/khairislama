5b8f5cd4940c6fdb9177856e335cf44e08072ef7
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-20 17:31:38 +0100

    feat: :sparkles: finished contact card

---
+++ portfolio/src/components/layout/contactCard/CardAvatar.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function CardAvatar() {
+  return (
+    <div className="aspect-square w-24 rounded-full flex items-center justify-center bg-gradient-to-b from-[#383838] to-background-darker">
+      <div className="aspect-square w-20 rounded-full flex items-center justify-center bg-gradient-to-b from-[#424242] to-background-darker">
+        <div className="aspect-square w-16 rounded-full flex items-center justify-center bg-gradient-to-b from-[#616161] to-background-darker overflow-hidden">
+          <Image
+            src="/avatars/avatar-3.png"
+            alt="@khairislama"
+            width={100}
+            height={100}
+            className="w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardAvatar;
