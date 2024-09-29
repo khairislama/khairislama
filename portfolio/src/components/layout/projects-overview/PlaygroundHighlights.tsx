9c4f2b8fb1e7d8fe711ab1ca5225899a5a4a7593
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-29 14:46:14 +0100

    fix: :bug: added translation in project overview

--- portfolio/src/components/layout/projects-overview/PlaygroundHighlights.tsx
+++ portfolio/src/components/layout/projects-overview/PlaygroundHighlights.tsx
@@ -1,26 +1,35 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 function PlaygroundHighlights() {
+  const translations = useTranslations("ProjectsOverview");
+
   return (
     <div className="flex flex-col gap-2">
       <div className="grid grid-cols-3 w-full mt-4 gap-1">
         <div>
-          <p className="text-xl font-semibold">Educational</p>
-          <p className="text-xs">Learn 3D web development interactively.</p>
-        </div>
-        <div>
-          <p className="text-xl font-semibold">Blender Integration</p>
-          <p className="text-xs">Seamlessly import Blender models.</p>
-        </div>
-        <div>
-          <p className="text-xl font-semibold">Visual Effects</p>
-          <p className="text-xs">
-            Advanced postprocessing for cinematic scenes.
+          <p className="text-xl font-semibold">
+            {translations("playground-highlight-1-title")}
           </p>
+          <p className="text-xs">{translations("playground-highlight-1")}</p>
+        </div>
+        <div>
+          <p className="text-xl font-semibold">
+            {translations("playground-highlight-2-title")}
+          </p>
+          <p className="text-xs">{translations("playground-highlight-2")}</p>
+        </div>
+        <div>
+          <p className="text-xl font-semibold">
+            {translations("playground-highlight-3-title")}
+          </p>
+          <p className="text-xs">{translations("playground-highlight-3")}</p>
         </div>
       </div>
       <div className="flex items-center justify-start gap-1 xl:gap-4 col-span-2">
-        <p className="underline underline-offset-4">Backend Stack: </p>
+        <p className="underline underline-offset-4">
+          {translations("backend-stack")}:{" "}
+        </p>
         <div className="flex items-center gap-1 sm:gap-3 md:gap-1 xl:gap-3">
           <Image
             src="/skills/nextjs.png"
--- portfolio/src/components/layout/projects-overview/StrongCastHighlights.tsx
+++ portfolio/src/components/layout/projects-overview/StrongCastHighlights.tsx
@@ -1,16 +1,23 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 function StrongCastHighlights() {
+  const translations = useTranslations("ProjectsOverview");
+
   return (
     <div className="flex flex-col gap-2">
       <div className="grid grid-cols-3 sm:grid-cols-10 md:grid-cols-3 lg:grid-cols-10 w-full mt-4 gap-1">
         <p className="text-4xl text-green-500 font-bold">A</p>
         <div className="sm:col-span-2 md:col-span-1 lg:col-span-2">
-          <p className="text-sm font-semibold xl:text-lg">Performance</p>
+          <p className="text-sm font-semibold xl:text-lg">
+            {translations("performance")}
+          </p>
           <p className="text-3xl font-bold text-green-500">98%</p>
         </div>
         <div className="sm:col-span-2 md:col-span-1 lg:col-span-2">
-          <p className="text-sm font-semibold xl:text-lg">Structure</p>
+          <p className="text-sm font-semibold xl:text-lg">
+            {translations("structure")}
+          </p>
           <p className="text-3xl font-bold text-green-500">100%</p>
         </div>
         <div className="sm:col-span-2 md:col-span-1 lg:col-span-2">
@@ -27,7 +34,9 @@ function StrongCastHighlights() {
         </div>
       </div>
       <div className="flex items-center justify-start gap-1 xl:gap-4 col-span-2">
-        <p className="underline underline-offset-4">Stack: </p>
+        <p className="underline underline-offset-4">
+          {translations("stack")}:{" "}
+        </p>
         <div className="flex items-center gap-1 sm:gap-3 md:gap-1 xl:gap-3">
           <Image
             src="/skills/nextjs.png"
