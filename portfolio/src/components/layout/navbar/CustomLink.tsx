6cf6953282399b4567a16655a4d4989b7448b1a2
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-09-29 12:35:02 +0100

    fix: :bug: making under construction nav links

--- portfolio/src/components/layout/navbar/CustomLink.tsx
+++ portfolio/src/components/layout/navbar/CustomLink.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Construction } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,10 +14,12 @@ const CustomLink = ({
   href,
   title,
   className = "",
+  disabled,
 }: {
   href: string;
   title: string;
   className?: string;
+  disabled?: boolean;
 }) => {
   const pathname = usePathname();
   const langPrefix = pathname.split("/")[1];
@@ -26,9 +35,21 @@ const CustomLink = ({
       href={fullLink}
       className={`${className} relative group hover:text-foreground ${
         isActive && "text-foreground"
-      }`}
+      } ${disabled && "cursor-not-allowed"}`}
     >
-      {title}
+      {disabled ? (
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>{title}</TooltipTrigger>
+            <TooltipContent className="bg-foreground flex items-center gap-2">
+              <Construction className="h-4 w-4" />
+              <p>under construction</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      ) : (
+        title
+      )}
 
       <span
         className={`h-[1px] inline-block w-0 bg-foreground absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
