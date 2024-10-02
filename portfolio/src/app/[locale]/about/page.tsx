90716a5f1e51e208889e73901f2367c3e7ff08db
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-02 22:11:56 +0100

    feat: :sparkles: added my journey section

--- portfolio/src/app/[locale]/about/page.tsx
+++ portfolio/src/app/[locale]/about/page.tsx
@@ -1,14 +1,15 @@
-import { PageHero } from "@/components/layout";
+import { MyJourney, PageHero } from "@/components/layout";
 import { useTranslations } from "next-intl";
 
 function AboutMePage() {
   const translations = useTranslations("About");
   return (
-    <main className="w-full overflow-hidden h-screen">
+    <main className="w-full overflow-hidden">
       <PageHero
         title={translations("title")}
         subtitle={translations("subtitle")}
       />
+      <MyJourney />
     </main>
   );
 }
--- portfolio/src/components/layout/index.ts
+++ portfolio/src/components/layout/index.ts
@@ -1,18 +1,22 @@
+import ContactCard from "./contactCard";
+import AboutCurrentWork from "./current-work";
 import Footer from "./footer";
 import Hero from "./hero";
 import KnowAboutMe from "./know-about-me";
+import MyJourney from "./my-journey";
 import Navbar from "./navbar";
 import PageHero from "./pageName";
 import ProjectsOverview from "./projects-overview";
 import Services from "./services";
 import Skills from "./skills";
 import Testimonials from "./testimonials";
-import ContactCard from "./contactCard";
 
 export {
   Footer,
+  AboutCurrentWork,
   Hero,
   KnowAboutMe,
+  MyJourney,
   Navbar,
   PageHero,
   ProjectsOverview,
