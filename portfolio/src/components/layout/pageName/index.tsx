f21ec69bdc60b6ee9df869778e335c0df640d71c
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-02 12:18:34 +0100

    feat: :construction: started working on about me page

---
+++ portfolio/src/components/layout/pageName/index.tsx
@@ -0,0 +1,15 @@
+function PageHero() {
+  return (
+    <section className="w-full max-w-7xl mx-auto my-24">
+      <h1 className="text-4xl font-semibold font-syne text-foreground tracking-wider">
+        About Me
+      </h1>
+      <p className="text-sm tracking-wide font-rubik text-justify mt-4">
+        Little Brief About Myself
+      </p>
+      <div className="h-[1px] w-full bg-foreground/70 mt-12" />
+    </section>
+  );
+}
+
+export default PageHero;
