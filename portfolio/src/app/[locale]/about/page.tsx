d57c41ed1710c2560b7e8e196d9c131535ba4929
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-02 21:27:59 +0100

    feat: :sparkles: Custom hero for pages created

--- portfolio/src/app/[locale]/about/page.tsx
+++ portfolio/src/app/[locale]/about/page.tsx
@@ -1,9 +1,14 @@
 import { PageHero } from "@/components/layout";
+import { useTranslations } from "next-intl";
 
 function AboutMePage() {
+  const translations = useTranslations("About");
   return (
     <main className="w-full overflow-hidden h-screen">
-      <PageHero />
+      <PageHero
+        title={translations("title")}
+        subtitle={translations("subtitle")}
+      />
     </main>
   );
 }
