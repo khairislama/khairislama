a2f0c91f7e13ff3915d931c6b2971727256dbbff
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-02 21:27:59 +0100

    feat: :sparkles: Custom hero for pages created

--- portfolio/src/components/layout/pageName/index.tsx
+++ portfolio/src/components/layout/pageName/index.tsx
@@ -1,13 +1,13 @@
-function PageHero() {
+function PageHero({ title, subtitle }: { title: string; subtitle: string }) {
   return (
-    <section className="w-full max-w-7xl mx-auto my-24">
+    <section className="w-full px-4 xl:px-0 max-w-7xl mx-auto my-10 md:my-16 lg:my-20 xl:my-24">
       <h1 className="text-4xl font-semibold font-syne text-foreground tracking-wider">
-        About Me
+        {title}
       </h1>
-      <p className="text-sm tracking-wide font-rubik text-justify mt-4">
-        Little Brief About Myself
+      <p className="text-sm tracking-wide font-rubik text-justify mt-3 sm:mt-4">
+        {subtitle}
       </p>
-      <div className="h-[1px] w-full bg-foreground/70 mt-12" />
+      <div className="h-[1px] w-full bg-foreground/70 mt-6" />
     </section>
   );
 }
