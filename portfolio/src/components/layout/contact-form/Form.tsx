6dd1fdf547453ab398cc2251fbac2ae3901fc4c6
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-03 18:22:48 +0100

    fix: :ambulance: fixed type

--- portfolio/src/components/layout/contact-form/Form.tsx
+++ portfolio/src/components/layout/contact-form/Form.tsx
@@ -14,7 +14,7 @@ export default function ContactForm() {
   const [error, setError] = useState("");
   const { toast } = useToast();
 
-  const handleSubmit = async (event: any) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsLoading(true);
 
