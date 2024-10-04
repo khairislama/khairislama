b0a7bd0da2aa32bdb6bc138595bd9c718cf313f1
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-04 21:50:41 +0100

    fix: :bug: fixed form and margin bottom in about page

--- portfolio/src/components/layout/contact-form/Form.tsx
+++ portfolio/src/components/layout/contact-form/Form.tsx
@@ -86,38 +86,6 @@ export default function ContactForm() {
       </div>
       <div className="mb-6">
         <label
-          htmlFor="name"
-          className="block mb-3 font-medium sm:font-semibold sm:text-lg lg:text-xl uppercase"
-        >
-          Name
-        </label>
-        <Input
-          id="name"
-          name="name"
-          type="text"
-          autoComplete="off"
-          required
-          className="w-full sm:h-12 lg:h-14 border-0 border-b-2"
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="name"
-          className="block mb-3 font-medium sm:font-semibold sm:text-lg lg:text-xl uppercase"
-        >
-          subject
-        </label>
-        <Input
-          id="name"
-          name="name"
-          type="text"
-          autoComplete="off"
-          required
-          className="w-full sm:h-12 lg:h-14 border-0 border-b-2"
-        />
-      </div>
-      <div className="mb-6">
-        <label
           htmlFor="message"
           className="block mb-3 font-medium sm:font-semibold sm:text-lg lg:text-xl uppercase"
         >
