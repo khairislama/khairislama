2a140d7d882556a04f401e0b1721bfb236edca0e
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-06 17:37:58 +0100

    feat: :sparkles: peparing the ground for dynamic tooltips

--- portfolio/src/components/layout/project-list/Overview.tsx
+++ portfolio/src/components/layout/project-list/Overview.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 
-function Overview() {
+function Overview({
+  image,
+}: {
+  image: {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+  };
+}) {
   return (
-    <div className="h-44 w-44 rounded-xl overflow-hidden">
+    <div className="h-72 w-96 rounded-xl overflow-hidden hidden md:block">
       <Image
-        src="/projects/strong-cast/metrix.webp"
-        alt="eclipse"
-        width={1269}
-        height={819}
-        className="object-cover w-full pointer-events-none brightness-125"
+        src={image.src}
+        alt={image.alt}
+        width={image.width}
+        height={image.height}
+        className="object-cover w-full"
       />
+      <div className="absolute top-0 left-0 w-full h-full bg-background/30 flex items-center justify-center shadow-[inset_1px_1px_150px_12px_rgba(0,0,0,0.9)]">
+        <h4 className="text-white font-semibold text-2xl">click for details</h4>
+      </div>
     </div>
   );
 }
