606ae97d43e54f7c95a8aa9e9811aac8ad2a04a1
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-09 17:08:33 +0100

    feat: :sparkles: defused singular components for single project and fixed info layout

--- portfolio/src/components/layout/project-info/index.tsx
+++ portfolio/src/components/layout/project-info/index.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import GithubOneProject from "./Github";
 import ExternalOneProject from "./External";
 import TagsOneProject from "./Tags";
+import StoryOneProject from "./Story";
+import InfoOneProject from "./Info";
+import ParallelImages from "./parallel";
 
 function ProjectInfo({ project }: { project: IProject }) {
   return (
@@ -23,60 +26,10 @@ function ProjectInfo({ project }: { project: IProject }) {
           <ExternalOneProject external_link={project.external_link} />
         </div>
       </div>
-      <div className="w-full mx-auto max-w-2xl my-12">
-        <h2 className="text-3xl md:text-4xl font-syne font-semibold tracking-wider">
-          Project Story
-        </h2>
-        <p className="text-lg mt-8 font-rubik">
-          The fact that photography has different meanings to different people
-          is one of the many components of its appeal. Photography is such an
-          important part of our life that it is now very difficult to imagine
-          the world without it.
-        </p>
-        <p className="text-lg mt-8 font-rubik">
-          We cannot imagine a wedding without the opportunity to capture it on
-          film, we would not be able to remember the growing up of children or
-          the holidays if we did not have pictures.
-        </p>
-        {/* <FeaturesOneProject /> */}
-        {/* <div className="flex flex-col gap-8">
-          <InfiniteScroll orientation="left" />
-          <InfiniteScroll orientation="right" />
-          <InfiniteScroll orientation="top" />
-          <InfiniteScroll orientation="bottom" />
-        </div> */}
-      </div>
+      <StoryOneProject />
       <TagsOneProject />
-      <div className="w-full mx-auto max-w-4xl grid grid-cols-3 gap-24 mt-10 font-syne">
-        <div className="flex flex-col items-center justify-center">
-          <h4 className="text-foreground text-lg font-semibold">Start date</h4>
-          <p className="text-lg">6 September 2024</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <h4 className="text-foreground text-lg font-semibold">Client</h4>
-          <p className="text-lg">Hutchinson</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <h4 className="text-foreground text-lg font-semibold">Role</h4>
-          <p className="text-lg">Backend developer</p>
-        </div>
-      </div>
-      <div className="flex gap-12 h-full my-10">
-        <Image
-          src={project.image.src}
-          alt={project.image.alt}
-          width={project.image.width}
-          height={project.image.height}
-          className="object-cover w-[48%]"
-        />
-        <Image
-          src={project.image.src}
-          alt={project.image.alt}
-          width={project.image.width}
-          height={project.image.height}
-          className="object-cover w-[48%]"
-        />
-      </div>
+      <InfoOneProject />
+      <ParallelImages project={project} />
     </section>
   );
 }
