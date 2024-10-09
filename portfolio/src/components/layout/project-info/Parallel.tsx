6b4e9eff27f60fdade07e65156ad60f13970f192
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-09 17:24:10 +0100

    feat: :sparkles: added the exploration text

--- portfolio/src/components/layout/project-info/Parallel.tsx
+++ portfolio/src/components/layout/project-info/Parallel.tsx
@@ -3,21 +3,26 @@ import Image from "next/image";
 
 function ParallelImages({ project }: { project: IProject }) {
   return (
-    <div className="flex gap-12 h-full my-10">
-      <Image
-        src={project.image.src}
-        alt={project.image.alt}
-        width={project.image.width}
-        height={project.image.height}
-        className="object-cover w-[48%]"
-      />
-      <Image
-        src={project.image.src}
-        alt={project.image.alt}
-        width={project.image.width}
-        height={project.image.height}
-        className="object-cover w-[48%]"
-      />
+    <div className="flex flex-col md:flex-row gap-4 md:gap-8 h-full my-10">
+      <div className="w-full overflow-hidden rounded-xl">
+        <Image
+          src={project.image.src}
+          alt={project.image.alt}
+          width={project.image.width}
+          height={project.image.height}
+          className="object-cover"
+        />
+      </div>
+
+      <div className="w-full overflow-hidden rounded-xl">
+        <Image
+          src={project.image.src}
+          alt={project.image.alt}
+          width={project.image.width}
+          height={project.image.height}
+          className="object-cover"
+        />
+      </div>
     </div>
   );
 }
