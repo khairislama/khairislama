88c63ca4bb88dd736ebee92237fa29b77dcfd008
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-09 12:14:18 +0100

    feat: :sparkles: added infinite scroll for tags

--- portfolio/src/components/layout/project-info/Features.tsx
+++ portfolio/src/components/layout/project-info/Features.tsx
@@ -1,24 +1,7 @@
-"use client";
-
-import { motion, Variants } from "framer-motion";
-
-const infiniteScroll: Variants = {
-  animate: {
-    left: "-40px",
-    transition: {
-      duration: 3,
-    },
-  },
-};
-
 function FeaturesOneProject() {
   const list = ["Blender", "Freelance", "solo", "web design", "next.js"];
   return (
-    <motion.ul
-      className="grid grid-cols-5 gap-10 mt-12 w-full relative"
-      variants={infiniteScroll}
-      animate="animate"
-    >
+    <ul className="grid grid-cols-5 gap-10 mt-12 w-full relative">
       {list.map((item, i) => (
         <li
           className="py-2 px-4 h-10 w-32 bg-foreground/40 rounded-full text-center absolute left-[100%] repeat-infinite"
@@ -27,7 +10,7 @@ function FeaturesOneProject() {
           {item}
         </li>
       ))}
-    </motion.ul>
+    </ul>
   );
 }
 
