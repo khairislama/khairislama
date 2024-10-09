a931cadbc6ee92514f76b41b3cc0f0953e62b5f8
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-09 17:39:53 +0100

    feat: :sparkles: stats

--- portfolio/src/components/layout/project-info/Stats.tsx
+++ portfolio/src/components/layout/project-info/Stats.tsx
@@ -1,7 +1,16 @@
 function StatsOneProject() {
   return (
-    <div>
-      <p className="text-7xl text-green-500 font-bold font-syne">A</p>
+    <div className="md:w-[50%] relative">
+      {/* <p className="text-[200px] text-green-500 font-bold font-syne absolute -top-[10%] w-full text-center">
+        A
+      </p> */}
+      {/* The 5 divs here */}
+      <div className="relative z-20 hidden md:flex items-center justify-center h-full">
+        <span className="text-5xl xl:text-7xl font-syne text-center font-bold">
+          NO
+          <br /> PERFORMANCE <br /> STATS
+        </span>
+      </div>
     </div>
   );
 }
