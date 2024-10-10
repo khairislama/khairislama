c40d8526c0553611a97d949a952af68a6239ff6e
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-10 16:27:35 +0100

    feat: :sparkles: added recommended projects section

--- portfolio/src/app/[locale]/projects/[slang]/page.tsx
+++ portfolio/src/app/[locale]/projects/[slang]/page.tsx
@@ -1,7 +1,10 @@
-import { PageHero, ProjectInfo } from "@/components/layout";
+import {
+  PageHero,
+  ProjectInfo,
+  RecommendedProjects,
+} from "@/components/layout";
 import { PROJECTS } from "@/lib/Projects";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 function SingleProjectPage({ params }: { params: { slang: string } }) {
   const slang = params.slang;
@@ -16,34 +19,7 @@ function SingleProjectPage({ params }: { params: { slang: string } }) {
         subtitle={translations("subtitle")}
       />
       <ProjectInfo project={project} />
-      <div className="flex w-full max-w-7xl mx-auto px-4 sm:px-6 xl:px-0 items-center justify-between gap-3">
-        <div className="flex items-end gap-2 md:gap-4">
-          <Image
-            src="/shapes/arrow-left.svg"
-            alt="Previous"
-            width={50}
-            height={13}
-            className="w-8 md:w-14"
-            sizes="100vw, (max-width: 1200px) 50vw,(max-width: 768px) 30vw"
-          />
-          <p className="font-semibold font-rubik text-sm md:text-lg text-foreground/90 uppercase">
-            Previous work
-          </p>
-        </div>
-        <div className="flex items-start gap-2 md:gap-4">
-          <p className="font-semibold font-rubik text-sm md:text-lg text-foreground/90 uppercase">
-            Next work
-          </p>
-          <Image
-            src="/shapes/arrow-right.svg"
-            alt="Next"
-            width={50}
-            height={13}
-            className="w-8 md:w-14"
-            sizes="100vw, (max-width: 1200px) 50vw,(max-width: 768px) 30vw"
-          />
-        </div>
-      </div>
+      <RecommendedProjects />
     </main>
   );
 }
