cd6c0b64673332558cfe09186352309c2768b2be
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-11 13:33:13 +0100

    fix: :bug: added github stats to the mix

--- portfolio/src/components/layout/project-info/GithubStats.tsx
+++ portfolio/src/components/layout/project-info/GithubStats.tsx
@@ -1,51 +1,50 @@
+import { IProject } from "@/lib/Projects";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
-function GithubStats() {
+function GithubStats({ project }: { project: IProject }) {
+  const { stats } = project;
+  const translations = useTranslations(`Projects`);
   return (
     <section className="w-full mx-auto max-w-4xl my-10">
       <h2 className="text-3xl md:text-4xl font-syne font-semibold tracking-wider md:text-center">
-        Formulation&apos;s stats
+        {translations("github-stats")}
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4 md:mt-10">
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Total star earned</p>
-          <p className="font-syne font-bold text-3xl">72.6k</p>
+          <p className="font-rubik">{translations("total-star-earned")}</p>
+          <p className="font-syne font-bold text-3xl"> {stats.stars} </p>
         </div>
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Total commits</p>
-          <p className="font-syne font-bold text-3xl">86</p>
+          <p className="font-rubik">{translations("total-commits")}</p>
+          <p className="font-syne font-bold text-3xl">{stats.commits}</p>
         </div>
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Total PRs</p>
-          <p className="font-syne font-bold text-3xl">14</p>
+          <p className="font-rubik">{translations("total-prs")}</p>
+          <p className="font-syne font-bold text-3xl">{stats.prs}</p>
         </div>
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Total issues</p>
-          <p className="font-syne font-bold text-3xl">7</p>
+          <p className="font-rubik">{translations("total-issues")}</p>
+          <p className="font-syne font-bold text-3xl">{stats.issues}</p>
         </div>
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Total tests</p>
-          <p className="font-syne font-bold text-3xl">64</p>
+          <p className="font-rubik">{translations("total-tests")}</p>
+          <p className="font-syne font-bold text-3xl">{stats.tests}</p>
         </div>
         <div className="w-full flex flex-col items-center justify-center text-xl gap-2">
-          <p className="font-rubik">Technologies</p>
+          <p className="font-rubik">{translations("technologies")}</p>
           <div className="w-full h-full flex items-center justify-center gap-4">
-            <Image
-              src="/icons/nextjs.png"
-              alt="NextJS"
-              width={300}
-              height={300}
-              className="w-8"
-              sizes="100vw, (max-width: 1200px) 50vw,(max-width: 768px) 30vw"
-            />
-            <Image
-              src="/skills/tailwindcss.png"
-              alt="Tailwind css"
-              width={89}
-              height={87}
-              className="w-8"
-              sizes="100vw, (max-width: 1200px) 50vw,(max-width: 768px) 30vw"
-            />
+            {stats.technologies.map((tech, i) => (
+              <Image
+                key={i}
+                src={tech.src}
+                alt={tech.alt}
+                width={50}
+                height={50}
+                className="w-8"
+                sizes="100vw, (max-width: 1200px) 50vw,(max-width: 768px) 30vw"
+              />
+            ))}
           </div>
         </div>
       </div>
