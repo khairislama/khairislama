226cc1378cc9ce90c80153cd631a034e8c91617c
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-11 09:59:51 +0100

    feat: :sparkles: fixed the first big chuck dynamically for one project

--- portfolio/src/components/layout/project-info/Story.tsx
+++ portfolio/src/components/layout/project-info/Story.tsx
@@ -1,24 +1,25 @@
 import { useTranslations } from "next-intl";
+import TagsOneProject from "./Tags";
+import InfoOneProject from "./Info";
+import { IProject } from "@/lib/Projects";
 
-function StoryOneProject({
-  stories,
-  slang,
-}: {
-  stories: number;
-  slang: string;
-}) {
+function StoryOneProject({ project }: { project: IProject }) {
   const translations = useTranslations(`Projects`);
-  const projectTranslations = useTranslations(`Project-${slang}`);
+  const projectTranslations = useTranslations(`Project-${project.slang}`);
   return (
-    <div className="w-full mx-auto max-w-2xl my-12">
-      <h2 className="text-3xl md:text-4xl font-syne font-semibold tracking-wider text-foreground/90">
-        {translations("project-story")}
-      </h2>
-      {Array.from({ length: stories }).map((_, i) => (
-        <p key={i} className="text-lg mt-8 font-rubik">
-          {projectTranslations(`story-${i + 1}`)}
-        </p>
-      ))}
+    <div className="w-full my-12">
+      <div className="mx-auto max-w-2xl w-full">
+        <h2 className="text-3xl md:text-4xl font-syne font-semibold tracking-wider text-foreground/90">
+          {translations("project-story")}
+        </h2>
+        {Array.from({ length: project.stories }).map((_, i) => (
+          <p key={i} className="text-lg mt-8 font-rubik">
+            {projectTranslations(`story-${i + 1}`)}
+          </p>
+        ))}
+      </div>
+      <TagsOneProject tags={project.tags} slang={project.slang} />
+      <InfoOneProject slang={project.slang} />
     </div>
   );
 }
