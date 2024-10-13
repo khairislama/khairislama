017f9531949f1022793f864a42ffc79accb98457
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-13 23:13:08 +0100

    feat: :sparkles: added project context

--- portfolio/src/components/layout/pageName/index.tsx
+++ portfolio/src/components/layout/pageName/index.tsx
@@ -1,7 +1,7 @@
 function PageHero({ title, subtitle }: { title: string; subtitle: string }) {
   return (
     <section className="w-full max-w-7xl mx-auto my-10 md:my-16 lg:my-20 xl:my-24 px-4 sm:px-6 xl:px-0">
-      <h1 className="text-4xl lg:text-6xl font-semibold font-syne text-foreground tracking-wider">
+      <h1 className="text-2xl sm:text-4xl lg:text-6xl font-semibold font-syne text-foreground tracking-wider">
         {title}
       </h1>
       <p className="text-lg tracking-wide font-rubik text-justify mt-3 sm:mt-4">
