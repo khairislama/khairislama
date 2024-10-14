c20bf5cd5f3684eb1797b861de101613d7265bdb
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-14 17:24:31 +0100

    fix: :ambulance: fixed layout and removed play navigation

--- portfolio/src/components/layout/navbar/Navigation.tsx
+++ portfolio/src/components/layout/navbar/Navigation.tsx
@@ -9,7 +9,7 @@ function Navigation() {
     <nav className="flex items-center justify-center gap-6 uppercase ">
       <CustomLink href="/" title={translations("home")} />
       <CustomLink href="/about" title={translations("about")} />
-      <CustomLink href="/play" title={translations("play")} disabled />
+      {/* <CustomLink href="/play" title={translations("play")} disabled /> */}
       <CustomLink href="/projects" title={translations("projects")} />
     </nav>
   );
