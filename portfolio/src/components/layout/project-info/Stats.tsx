26de626bd740b39ab3139d5f223a4a773fdfbca7
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-14 16:10:47 +0100

    fix: :ambulance: fixed key for each span

--- portfolio/src/components/layout/project-info/Stats.tsx
+++ portfolio/src/components/layout/project-info/Stats.tsx
@@ -45,15 +45,15 @@ function StatsOneProject({ project }: { project: IProject }) {
         </div>
       ) : (
         <div className="relative z-20 hidden md:flex items-center justify-center h-full">
-          <span className="text-5xl xl:text-7xl font-syne text-center font-bold uppercase">
+          <p className="text-5xl xl:text-7xl font-syne text-center font-bold uppercase">
             {translations("no-performance")
               .split(" ")
-              .map((word) => (
-                <>
+              .map((word, i) => (
+                <span key={i}>
                   {word} <br />
-                </>
+                </span>
               ))}
-          </span>
+          </p>
         </div>
       )}
     </div>
