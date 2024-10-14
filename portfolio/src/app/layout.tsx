b611603a5011941f61fa91813908624fdeb2c906
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-10-14 23:13:47 +0100

    fix: :bug: removed static params

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -6,8 +6,6 @@ export default function RootLayout({
   return children;
 }
 
-// TODO: The SEO image
-// TODO: Check images alt
 // TODO: Add robot xml
 // TODO: Add animated texts
 // TODO: Add stars and a switch color in the end for the footer
