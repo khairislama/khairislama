f02b23270f15ae6d47895b9bad96d30b80e2cfe3
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-11-03 22:58:09 +0100

    feat: :sparkles: added opengraph image and fixed SEO

--- portfolio/src/app/[locale]/page.tsx
+++ portfolio/src/app/[locale]/page.tsx
@@ -7,31 +7,7 @@ import {
   Testimonials,
 } from "@/components/layout";
 import Services from "@/components/layout/services";
-import { Metadata } from "next";
 import { useTranslations } from "next-intl";
-
-export const metadata: Metadata = {
-  title: "Khairi Slama | Full-Stack Engineer | Web Development Portfolio",
-  description:
-    "Explore Khairi Slama's web development portfolio, with a focus on security, 3D web, and performance optimization. Available for opportunities in Luxembourg and France.",
-  keywords:
-    "Khairi Slama, Full-Stack Engineer, Web Development, Next.js, Tailwind, Freelance, Luxembourg, France, Security, 3D Blender, Tunisian Engineer, Performance, Web Solutions",
-  openGraph: {
-    title: "Khairi Slama | Full-Stack Engineer",
-    description:
-      "Showcasing Khairi's web development projects, security-focused solutions, and 3D web experiences.",
-    url: "https://khairislama.vercel.app", // Ensure the domain is accurate
-    // images: [
-    //   {
-    //     url: "https://khairislama.vercel.app/icons/og-image.svg", // Full URL for better sharing
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "Khairi Slama Portfolio",
-    //   },
-    // ],
-    siteName: "Khairi Slama Portfolio",
-  },
-};
 
 export default function Home() {
   const translations = useTranslations("");
